fff8e8d7001e76a1698b8b14ba00c0425d7cc42b
= <=>
2025-02-09 21:57:04 +0530

    sdpt changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import User from './User'
 import CreateUser from './CreateUser'
 import UpdateUser from './UpdateUser'
 import Welcom from './Welcom';
+import Sdpt from './Sdpt';
 
 
 
 function App() {
   return (
     <>
+      <Sdpt></Sdpt>
       <Welcom></Welcom>
       <Routes>
         <Route path="/" element={<User />} />
